b25e236a1837afc9d67ca1f96f05cfff58418f0b
Shane Banks <shane.banks02@gmail.com>
2022-06-26 18:43:04 -0500

    Typecasting on square in main

--- src/Shapes/Main.java
+++ src/Shapes/Main.java
@@ -2,7 +2,7 @@ package Shapes;
 
 public class Main {
 	public static void main(String[] args) {
-		displayShape(new Square());
+		displayShape(new Triangle());
 
 	}
 
@@ -10,9 +10,11 @@ public class Main {
 		System.out.println(shape.getName());
 		shape.draw();
 		System.out.printf("Sides: %s\n", shape.getSideCount());
-		Square square = (Square) shape; //typecast square from Shape to Square
-		//shape.getShapeType();
-		System.out.println(square.getShapeType()); //above typecast to shape allows using a Square method
+		if (shape instanceof Square) { //if statement allows casting to square if created shape in Main is a square
+			Square square = (Square) shape; //typecast square from Shape to Square
+			//shape.getShapeType();
+			System.out.println(square.getShapeType()); //above typecast to shape allows using a Square method
+		}
 		System.out.println();
 	}
 
